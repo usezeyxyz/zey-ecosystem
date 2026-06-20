7d338ccc39287a2a5218ac62e04dbdf6e44317ef
usezeyxyz <enzidamroli@gmail.com>
2026-06-20 23:48:23 +0700

    Create BranchTree.tsx

---
+++ components/BranchTree.tsx
@@ -0,0 +1,23 @@
+export default function BranchTree() {
+
+  return (
+
+    <div>
+
+      <h2>
+        Reasoning Branches
+      </h2>
+
+      <ul>
+
+        <li>Analytical</li>
+        <li>Creative</li>
+        <li>Critical</li>
+
+      </ul>
+
+    </div>
+
+  );
+
+}
