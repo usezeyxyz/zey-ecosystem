47861af3b0246ee11e8e7ffd7e16a055282d5a98
usezeyxyz <enzidamroli@gmail.com>
2026-06-20 23:49:11 +0700

    Create PrivacyBanner.tsx

---
+++ PrivacyBanner.tsx
@@ -0,0 +1,19 @@
+export default function PrivacyBanner() {
+
+  return (
+
+    <div>
+
+      <h3>
+        Privacy First
+      </h3>
+
+      <p>
+        Nothing is written to disk.
+      </p>
+
+    </div>
+
+  );
+
+}
